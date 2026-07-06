b40f0d536028dd29fbf838d47703656141ceb589
Prince Kamriya <princekamriya321@gmail.com>
2026-07-06 15:57:37 +0530

    Time: 2 ms (28.79%), Space: 22.9 MB (52.27%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> ans(n,1);
+        for(int i =1; i<n; i++){
+            if(ratings[i]>ratings[i-1]){
+                ans[i] += ans[i-1];
+            }
+        }
+         for(int i = n-1; i>0; i--){
+            if(ratings[i-1]>ratings[i] && ans[i-1] <= ans[i]){
+                ans[i-1] = ans[i] + 1;
+            }
+        }
+      int  ct = accumulate(ans.begin(),ans.end(),0);
+        return ct;
+    }
+};
