98c28fd29e0705b736bee3a646665f68361f458f
Prince Kamriya <princekamriya321@gmail.com>
2026-07-06 21:34:03 +0530

    Time: 0 ms (100%), Space: 20.6 MB (58.49%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+       int n = nums.size();
+       int farthest = 0;
+       int currend = 0;
+       int jump = 0;
+       for(int i =0; i<n-1; i++){
+        farthest = max(farthest,nums[i]+i);
+        if(currend == i){
+            jump++;
+             currend = farthest;
+        }
+       } 
+       return jump;
+    }
+};
