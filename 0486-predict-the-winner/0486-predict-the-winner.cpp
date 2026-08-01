8fbe4e5ea6bce28dc0c8a2e890eae33f76a82e68
Prince Kamriya <princekamriya321@gmail.com>
2026-08-01 23:11:48 +0530

    Time: 0 ms (100%), Space: 10.4 MB (16.44%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    vector<vector<int>> dp;
+
+    int solve(int i,int j,vector<int>& nums){
+
+        if(i==j)
+            return nums[i];
+
+        if(dp[i][j]!=INT_MIN)
+            return dp[i][j];
+
+        int left = nums[i]-solve(i+1,j,nums);
+
+        int right = nums[j]-solve(i,j-1,nums);
+
+        return dp[i][j]=max(left,right);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+
+        int n=nums.size();
+
+        dp.assign(n,vector<int>(n,INT_MIN));
+
+        return (solve(0,n-1,nums)>=0);
+    }
+};
