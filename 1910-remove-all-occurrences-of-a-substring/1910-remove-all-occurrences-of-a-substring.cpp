00f50c936f51a546199a0cac9ae928c0a708ff23
Prince Kamriya <princekamriya321@gmail.com>
2026-08-06 00:27:14 +0530

    Time: 4 ms (15.66%), Space: 9.4 MB (72.52%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int n = s.size();
+        int m = part.size();
+      while(n> 0 && s.find(part) < n){
+        s.erase(s.find(part),m);
+      }
+      return s;
+    }
+};
