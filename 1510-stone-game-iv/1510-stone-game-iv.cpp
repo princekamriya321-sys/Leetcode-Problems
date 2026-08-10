f7b5e2e24d831d08751e3fd9374f901b6b527c16
Prince Kamriya <princekamriya321@gmail.com>
2026-08-10 17:47:37 +0530

    Time: 178 ms (17.16%), Space: 16.7 MB (42.05%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+vector<int> dp;
+bool solve(int n){
+    if(n <= 0) return 0;
+    if(dp[n] != -1) return dp[n];
+    for(int k =1; k*k<=n; k++){
+        if(solve(n-k*k) == false) return dp[n] = 1;
+    }
+    return false;
+}
+    bool winnerSquareGame(int n) {
+        dp.assign(n+1,-1);
+        return solve(n);
+    }
+};
