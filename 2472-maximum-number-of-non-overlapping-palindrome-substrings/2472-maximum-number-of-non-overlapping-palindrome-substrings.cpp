9babb4f505529c365bf662a919e8e7eb5352e5dd
Prince Kamriya <princekamriya321@gmail.com>
2026-09-15 10:58:58 +0530

    Time: 862 ms (10.21%), Space: 218 MB (13.61%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<vector<bool>> isPalindrome;
+    int solve(const string& s, int k, int i, int j, vector<vector<int>>& t) {
+        int n = s.size();
+        if (i >= n || j >= n) 
+            return 0;
+            
+        if (t[i][j] != -1) 
+            return t[i][j];
+
+        if (isPalindrome[i][j]) { 
+            int growWindow  = solve(s, k, i, j + 1, t);
+            int takeIt      = 1 + solve(s, k, j + 1, j + k, t);
+            int slideWindow = solve(s, k, i + 1, j + 1, t);
+
+            return t[i][j] = max({growWindow, takeIt, slideWindow});
+        }
+
+        int slideWindow = solve(s, k, i + 1, j + 1, t);
+        int growWindow  = solve(s, k, i, j + 1, t);
+
+        return t[i][j] = max(slideWindow, growWindow);
+    }
+
+    int maxPalindromes(string s, int k) {
+        int n = s.size();
+        if (k == 1)
+            return n;
+
+        isPalindrome.assign(n+1, vector<bool>(n+1));
+        for(int L = 1; L <= n; L++) {
+            for(int i = 0; i+L <= n; i++) {
+                int j = i + L - 1;
+
+                if(i == j) {
+                    isPalindrome[i][i] = true;
+                } else if(i+1 == j) {
+                    isPalindrome[i][j] = (s[i] == s[j]);
+                } else {
+                    isPalindrome[i][j] = ((s[i] == s[j]) && isPalindrome[i+1][j-1] == true);
+                }
+            }
+        }
+
+        vector<vector<int>> t(n, vector<int>(n, -1));
+
+        return solve(s, k, 0, k - 1, t);
+    }
+};
